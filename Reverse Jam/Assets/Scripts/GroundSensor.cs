43f43929a3e3ac808df294338e15ad3abfe9f08f
Pascal Han <isabelle_han@yahoo.com>
2021-04-15 18:49:07 -0700

    a bunch of stuff
    created basic level + background + colliders
    created basic player movement
    created scene Game
    added tag Terrain
    created folder Scripts
    created scripts PlayerControl, GroundSensor
    created basic player character (using placeholder graphic)

---
+++ Reverse Jam/Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    GameObject Player;
+    public bool playerGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Terrain")
+        {
+            Player.GetComponent<PlayerControl>().grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Terrain")
+        {
+            Player.GetComponent<PlayerControl>().grounded = false;
+        }
+    }
+}
---
+++ Reverse Jam/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public bool grounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+    }
+}
