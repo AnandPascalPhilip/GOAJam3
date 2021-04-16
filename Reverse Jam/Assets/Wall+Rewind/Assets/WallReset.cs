39045083489ce0e891204b146e5b667f4a25a37f
ptriolo <21triphi@hawken.edu>
2021-04-16 16:40:47 -0400

    Created Build

---
+++ Reverse Jam/Assets/Wall+Rewind/Assets/WallReset.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallReset : MonoBehaviour {
+
+    public bool isRewinding = false;
+
+    List<Vector3> positions;
+
+    void Start() {
+        positions = new List<Vector3>();
+
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return))
+            StartRewind();
+        if (Input.GetKeyUp(KeyCode.Return))
+            StopRewind();
+    }
+
+    void FixedUpdate ()
+    {
+        if (isRewinding == true)
+            Rewind();
+        else
+            Record();
+    }
+
+    void Rewind ()
+    {
+        Debug.Log("Movingback");
+        transform.position = positions[0];
+        positions.RemoveAt(0);
+    }
+
+    void Record ()
+    {
+        positions.Insert(0,transform.position);
+        Debug.Log("Logging");
+    }
+
+    public void StartRewind ()
+    {
+        isRewinding = true;
+        Debug.Log("Rewind");
+    }
+
+    public void StopRewind ()
+    {
+        isRewinding = false;
+        Debug.Log("Rewindstopped");
+    }
+
+
+}
