969668e67ae5fdf5fb132db11ff22109990bd971
Pascal Han <isabelle_han@yahoo.com>
2021-04-18 16:04:47 -0700

    attempted build

---
+++ Reverse Jam/Temp/StagingArea/Data/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,127 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+
+
+
+// 0x00000001 System.Void GroundSensor::Start()
+extern void GroundSensor_Start_m8C29DF1A1F136F4268FEBCD2EEDC69E3AA16070E (void);
+// 0x00000002 System.Void GroundSensor::Update()
+extern void GroundSensor_Update_mADBF3643772249DC7103FECB4882CF85F0872A38 (void);
+// 0x00000003 System.Void GroundSensor::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void GroundSensor_OnCollisionEnter2D_mCA5EB9F2AC6C6C9B16F72AB85FE6A8556D02E87C (void);
+// 0x00000004 System.Void GroundSensor::OnCollisionExit2D(UnityEngine.Collision2D)
+extern void GroundSensor_OnCollisionExit2D_mF3DFD1EA6CB875613858B5B50D96721F6DC5895E (void);
+// 0x00000005 System.Void GroundSensor::.ctor()
+extern void GroundSensor__ctor_m432D7BDAE01F11FF07D476BDEF256DFFF44FD34D (void);
+// 0x00000006 System.Void OutOfBoundsRespawn::Start()
+extern void OutOfBoundsRespawn_Start_m0F057C7CB13ED2947DC9F4916C517339B4006754 (void);
+// 0x00000007 System.Void OutOfBoundsRespawn::Update()
+extern void OutOfBoundsRespawn_Update_m9EE2198D872E8B749B1E6DB16CCF82033AD86186 (void);
+// 0x00000008 System.Void OutOfBoundsRespawn::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void OutOfBoundsRespawn_OnTriggerEnter2D_mA26C39A79581193FBF913FDDA62368B2853A09B6 (void);
+// 0x00000009 System.Void OutOfBoundsRespawn::.ctor()
+extern void OutOfBoundsRespawn__ctor_mCF2AA50B72782E8CE12421855ED201C8FAD1E858 (void);
+// 0x0000000A System.Void PlayerControl::Start()
+extern void PlayerControl_Start_m14162C2597FB3B8D7A46DC14DBC0DD9CB1D718AB (void);
+// 0x0000000B System.Void PlayerControl::Update()
+extern void PlayerControl_Update_mE0B426A84F641FBA9D80C9922F4B3F163BC9168C (void);
+// 0x0000000C System.Void PlayerControl::Jump()
+extern void PlayerControl_Jump_m12D5F6AA5C3F09D8B88002CD9B92DEDA790CFF7C (void);
+// 0x0000000D System.Void PlayerControl::.ctor()
+extern void PlayerControl__ctor_m01F3ADA98800180ABD6CB94057B526D688AAC30D (void);
+// 0x0000000E System.Void WallReset::Start()
+extern void WallReset_Start_m7F5652D0D2EF7ECB5DE2D2461431B75AA87D7AD0 (void);
+// 0x0000000F System.Void WallReset::Update()
+extern void WallReset_Update_m6677A86C8884C899E32A5C129E8D26C89C4F9F34 (void);
+// 0x00000010 System.Void WallReset::FixedUpdate()
+extern void WallReset_FixedUpdate_m98BAEA59408C3FDDC0880F6759771763AE115956 (void);
+// 0x00000011 System.Void WallReset::Rewind()
+extern void WallReset_Rewind_m1238DF84A0FEF8AABD08A339A6E7128A112350E3 (void);
+// 0x00000012 System.Void WallReset::Record()
+extern void WallReset_Record_m63F4E404A86DDB2B0317ACED95F40CB0119D647F (void);
+// 0x00000013 System.Void WallReset::StartRewind()
+extern void WallReset_StartRewind_mD89B882F230FD8077594319F042F7DE8DAA61AFE (void);
+// 0x00000014 System.Void WallReset::StopRewind()
+extern void WallReset_StopRewind_m04F1A3AFC68015AB3891926BD8CCE11464607EB1 (void);
+// 0x00000015 System.Void WallReset::.ctor()
+extern void WallReset__ctor_m1D59344F98822BFAD39EF3742E9149F1D68B5E3B (void);
+static Il2CppMethodPointer s_methodPointers[21] = 
+{
+	GroundSensor_Start_m8C29DF1A1F136F4268FEBCD2EEDC69E3AA16070E,
+	GroundSensor_Update_mADBF3643772249DC7103FECB4882CF85F0872A38,
+	GroundSensor_OnCollisionEnter2D_mCA5EB9F2AC6C6C9B16F72AB85FE6A8556D02E87C,
+	GroundSensor_OnCollisionExit2D_mF3DFD1EA6CB875613858B5B50D96721F6DC5895E,
+	GroundSensor__ctor_m432D7BDAE01F11FF07D476BDEF256DFFF44FD34D,
+	OutOfBoundsRespawn_Start_m0F057C7CB13ED2947DC9F4916C517339B4006754,
+	OutOfBoundsRespawn_Update_m9EE2198D872E8B749B1E6DB16CCF82033AD86186,
+	OutOfBoundsRespawn_OnTriggerEnter2D_mA26C39A79581193FBF913FDDA62368B2853A09B6,
+	OutOfBoundsRespawn__ctor_mCF2AA50B72782E8CE12421855ED201C8FAD1E858,
+	PlayerControl_Start_m14162C2597FB3B8D7A46DC14DBC0DD9CB1D718AB,
+	PlayerControl_Update_mE0B426A84F641FBA9D80C9922F4B3F163BC9168C,
+	PlayerControl_Jump_m12D5F6AA5C3F09D8B88002CD9B92DEDA790CFF7C,
+	PlayerControl__ctor_m01F3ADA98800180ABD6CB94057B526D688AAC30D,
+	WallReset_Start_m7F5652D0D2EF7ECB5DE2D2461431B75AA87D7AD0,
+	WallReset_Update_m6677A86C8884C899E32A5C129E8D26C89C4F9F34,
+	WallReset_FixedUpdate_m98BAEA59408C3FDDC0880F6759771763AE115956,
+	WallReset_Rewind_m1238DF84A0FEF8AABD08A339A6E7128A112350E3,
+	WallReset_Record_m63F4E404A86DDB2B0317ACED95F40CB0119D647F,
+	WallReset_StartRewind_mD89B882F230FD8077594319F042F7DE8DAA61AFE,
+	WallReset_StopRewind_m04F1A3AFC68015AB3891926BD8CCE11464607EB1,
+	WallReset__ctor_m1D59344F98822BFAD39EF3742E9149F1D68B5E3B,
+};
+static const int32_t s_InvokerIndices[21] = 
+{
+	23,
+	23,
+	26,
+	26,
+	23,
+	23,
+	23,
+	26,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+	23,
+};
+extern const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharpCodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	21,
+	s_methodPointers,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+};
