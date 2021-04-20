575a25c7853fa1f77afa151dd88ca6b532015102
ptriolo <21triphi@hawken.edu>
2021-04-19 22:23:56 -0400

    Edited GlobalRewind, added LocalRewind mechanics.

---
+++ Reverse Jam/Assets/Scripts/PlayerReset.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReset : MonoBehaviour
+{
+    public bool isLocalRewinding = false;
+    List<Vector3> positions;
+    void Start()
+    {
+        positions=new List<Vector3>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+            StartRewind();
+        if (Input.GetKeyUp(KeyCode.Backspace))
+            StopRewind();
+    }
+    void FixedUpdate(){
+        if (isLocalRewinding == true)
+            Rewind();
+        else
+            Record();
+    }
+
+    void Rewind(){
+        Debug.Log("Movingback");
+        transform.position = positions[0];
+        positions.RemoveAt(0);
+    }
+    void Record(){
+        positions.Insert(0,transform.position);
+        Debug.Log("Logging");
+    }
+        public void StartRewind ()
+    {
+        isLocalRewinding = true;
+        Debug.Log("Rewind");
+    }
+    public void StopRewind ()
+    {
+        isLocalRewinding = false;
+        Debug.Log("Rewindstopped");
+    }
+}
---
+++ Reverse Jam/Assets/Scripts/WallReset.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallReset : MonoBehaviour {
+
+    public bool isGlobalRewinding = false;
+
+    List<Vector3> positions;
+    List<Quaternion> rotations;
+
+    void Start() {
+        positions = new List<Vector3>();
+        rotations = new List<Quaternion>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return))
+            StartRewind();
+        if (Input.GetKeyUp(KeyCode.Return))
+            StopRewind();
+    }
+
+    void FixedUpdate ()
+    {
+        if (isGlobalRewinding == true)
+            Rewind();
+        else
+            Record();
+    }
+
+    void Rewind ()
+    {
+    //    Debug.Log("Movingback");
+        transform.position = positions[0];
+        transform.rotation = rotations[0];
+        positions.RemoveAt(0);
+        rotations.RemoveAt(0);
+    }
+
+    void Record ()
+    {
+        positions.Insert(0,transform.position);
+        rotations.Insert(0, transform.rotation);
+    //    Debug.Log("Logging");
+    }
+
+    public void StartRewind ()
+    {
+        isGlobalRewinding = true;
+    //    Debug.Log("Rewind");
+    }
+
+    public void StopRewind ()
+    {
+        isGlobalRewinding = false;
+    //    Debug.Log("Rewindstopped");
+    }
+}
