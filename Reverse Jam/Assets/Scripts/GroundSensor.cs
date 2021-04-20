740c977ee8ae7e96970b15555c7ef7256187f798
ptriolo <21triphi@hawken.edu>
2021-04-20 14:49:39 -0400

    Disabled Player Movement for Local Rewind | Fixed ground detection for walls and ungrounding bug

--- Reverse Jam/Assets/Scripts/GroundSensor.cs
+++ Reverse Jam/Assets/Scripts/GroundSensor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GroundSensor : MonoBehaviour
 {
     GameObject Player;
+    public int grounds=0;
     public bool playerGrounded;
 
     // Start is called before the first frame update
@@ -12,26 +13,21 @@ public class GroundSensor : MonoBehaviour
     {
         Player = gameObject.transform.parent.gameObject;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Terrain")
         {
+            grounds++;
             Player.GetComponent<PlayerControl>().grounded = true;
         }
     }
-
-    private void OnCollisionExit2D(Collision2D collision)
+        private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Terrain")
-        {
-            Player.GetComponent<PlayerControl>().grounded = false;
+        if (collision.collider.tag == "Terrain"){
+        grounds--;
+            if(grounds==0){   
+                Player.GetComponent<PlayerControl>().grounded = false;
+            }
         }
     }
 }
--- Reverse Jam/Assets/Scripts/PlayerControl.cs
+++ Reverse Jam/Assets/Scripts/PlayerControl.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    PlayerReset playerReset;
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
     public bool grounded = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerReset = GetComponent<PlayerReset>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
-        if(Input.GetButtonDown("Jump") && grounded)
-        {
-            Jump();
+        if (!playerReset.isLocalRewinding){
+        
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * moveSpeed;
+            if(Input.GetButtonDown("Jump") && grounded)
+            {
+                Jump();
+            }
         }
     }
 
